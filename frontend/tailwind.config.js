68c212ba611f0139d482b172e681c58c413e882f
gabyr <gabrielitorg@gmail.com>
2024-10-02 16:46:52 -0300

    se sigue intentando la pag principal

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./frontend/src/**/*.{html, js}"],
   theme: {
     extend: {
+      img: {
+      width : '2px',
+      height: '2px',
+      },
       fontFamily:{
         "haken": ['"Hanken Grotesk"', 'sans-serif']
       },
