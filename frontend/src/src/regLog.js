256e9044bf65168a26048fd0ab4761d9d1ce0bdc
unknown <zenagsuma5@gmail.com>
2024-10-10 16:25:47 -0300

    nuevamente se reviso el register y se hicieron modificaciones en el login - backend

--- frontend/src/src/regLog.js
+++ frontend/src/src/regLog.js
@@ -1,21 +1,46 @@
 const registerForm = document.querySelector("#reg-form");
+
 registerForm.addEventListener("submit", async (event) => {
   event.preventDefault();
+  const name = document.getElementById("nombre").value;
+  const surname = document.getElementById("apellido").value;
+  const dni = document.getElementById("dni").value;
+  const phoneNumber = document.getElementById("telefono").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("contrasena").value;
+  const address = document.getElementById("localidad").value;
 
-  const formData = new FormData(event.target);
-  const inpO = Object.fromEntries(formData.entries());
+  const inpO = {
+    name: name,
+    surname: surname,
+    dni: dni,
+    phoneNumber: phoneNumber,
+    email: email,
+    password: password,
+    address: address,
+  };
 
   console.log(inpO);
+
   try {
-    const asd = await fetch("http://localhost:4000/register", {
+    const response = await fetch("http://localhost:4000/register", {
       method: "POST",
       body: JSON.stringify(inpO),
       headers: {
         "Content-Type": "application/json",
       },
     });
-    console.log(asd.json());
+
+    const data = await response.json();
+    console.log(data);
+
+    if (response.ok) {
+      window.location.href = "login.html";
+    } else {
+      alert("Error al intentar registrarse: intente de nuevo");
+    }
   } catch (error) {
     console.log(error, "Cannot create connection");
+    alert("Error de conexión. Por favor, intenta más tarde.");
   }
 });
