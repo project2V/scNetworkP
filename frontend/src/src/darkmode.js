19115da5b480ed90a09b5c00a5c320b65b7afc7d
gabyr <gabrielitorg@gmail.com>
2024-10-31 16:48:39 -0300

    modal de publiaciones

--- frontend/src/src/darkmode.js
+++ frontend/src/src/darkmode.js
@@ -21,3 +21,20 @@ function load() {
 function store(value) {
   localStorage.setItem("darkmode", value);
 }
+document.getElementById("hamburger").addEventListener("click", function() {
+  document.querySelector(".menu").classList.toggle("active");
+});
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("myBtn");
+let span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
