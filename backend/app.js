8b6c4bca8043412461bd926cf0d4dda3d5a0d3e4
Gabriel_RXGV <gabrielitorg@gmail.com>
2024-12-01 13:45:18 -0300

    se estilizo el contenedor principal

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ import path from "node:path";
 const app = express();
 
 const corsOptions = {
-  origin: "http://127.0.0.1:5500",
+  origin: "http://127.0.0.1:5501",
   optionsSuccessStatus: 200,
   credentials: true,
 };
